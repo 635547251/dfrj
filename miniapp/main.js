91859c34bc8f6b59bca792880a55b9393b4cc1db
Phil666 <45195707+635547251@users.noreply.github.com>
2024-03-07 16:08:27 +0800

    uniapp构建

---
+++ miniapp/main.js
@@ -0,0 +1,24 @@
+import App from './App'
+import uviewPlus from 'uview-plus'
+
+// #ifndef VUE3
+import Vue from 'vue'
+import './uni.promisify.adaptor'
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+  ...App
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  app.use(uviewPlus)
+  return {
+    app
+  }
+}
+// #endif
